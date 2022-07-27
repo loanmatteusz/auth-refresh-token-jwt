158ae0e1b1f574a160ba093a099bf73213028beb
Loan Matteus <loan.matteusz@gmail.com>
2022-07-27 16:16:18 -0300

    [feat] create middleware and routes of courses for tests

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from 'jsonwebtoken';
+
+export function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+  const authToken = request.headers.authorization;
+
+  if (!authToken) return response.status(401).json({
+    message: "Token is missing!"
+  });
+
+  const [_, token] = authToken.split(" ");
+
+  try {
+    verify(token, process.env.TOKEN_SECRET);
+    return next();
+  }
+  catch (err) {
+    return response.status(401).json({
+      message: "Token invalid!"
+    });
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { AuthenticateUserController } from './useCases/authenticateUser/AuthenticateUserContoller';
 import { CreateUserController } from './useCases/createUser/CreateUserController';
+import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
 
 const router = Router();
 
@@ -9,5 +10,16 @@ const createUserController = new CreateUserController();
 
 router.post('/login', authenticateUserContoller.handle);
 router.post('/users', createUserController.handle);
+
+// Route for Auth token tests
+router.get('/courses', ensureAuthenticated, (_, response) => {
+  return response.json([
+    { id: 1, courseName: 'NodeJS' },
+    { id: 2, courseName: 'ReactJS' },
+    { id: 3, courseName: 'AngularJS' },
+    { id: 4, courseName: 'Flutter' },
+    { id: 5, courseName: 'Elixir' },
+  ]);
+});
 
 export { router }
