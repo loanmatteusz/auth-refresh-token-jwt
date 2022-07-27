09850bdbfed9e9600b66dfc6b83888f8049767e6
Loan Matteus <loan.matteusz@gmail.com>
2022-07-27 19:47:05 -0300

    [feat] create RefreshTokenUserController

---
+++ src/useCases/refreshTokenUser/RefreshTokenUserController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import { RefreshTokenUserUseCase } from "./RefreshTokenUserUseCase";
+
+class RefreshTokenUserController {
+  async handle(request: Request, response: Response) {
+    const { refresh_token } = request.body;
+
+    const refreshTokenUserUseCase = new RefreshTokenUserUseCase();
+    const token = await refreshTokenUserUseCase.execute(refresh_token);
+
+    return response.json(token);
+  }
+}
+
+export { RefreshTokenUserController }
