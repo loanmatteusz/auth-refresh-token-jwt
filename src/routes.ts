774c9849dabaff6df376ce2799a2010bffd0ce98
Loan Matteus <loan.matteusz@gmail.com>
2022-07-27 19:48:20 -0300

    [feat] create refresh token route

--- src/routes.ts
+++ src/routes.ts
@@ -2,14 +2,17 @@ import { Router } from 'express';
 import { AuthenticateUserController } from './useCases/authenticateUser/AuthenticateUserContoller';
 import { CreateUserController } from './useCases/createUser/CreateUserController';
 import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
+import { RefreshTokenUserController } from './useCases/refreshTokenUser/RefreshTokenUserController';
 
 const router = Router();
 
 const authenticateUserContoller = new AuthenticateUserController();
 const createUserController = new CreateUserController();
+const refreshTokenUserController = new RefreshTokenUserController();
 
 router.post('/login', authenticateUserContoller.handle);
 router.post('/users', createUserController.handle);
+router.post('/refresh-token', refreshTokenUserController.handle);
 
 // Route for Auth token tests
 router.get('/courses', ensureAuthenticated, (_, response) => {
